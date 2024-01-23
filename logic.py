3f72ad3dc68cdd5ef457c54ff31a3c30f2293522
MartynasPupkus <pupkus.martynas@gmail.com>
2024-01-23 14:58:49 +0200

    su bandymu

--- logic.py
+++ logic.py
@@ -18,14 +18,14 @@ def start_game():
 
 def add_new_2(mat):
 
-	r = random.randint(0, 3)
-	c = random.randint(0, 3)
+    r = random.randint(0, 3)
+    c = random.randint(0, 3)
 
-	while(mat[r] != 0):
-		r = random.randint(0, 3)
-		c = random.randint(0, 3)
+    while (mat[r][c] != 0):
+        r = random.randint(0, 3)
+        c = random.randint(0, 3)
 
-	mat[r] = 2
+    mat[r][c] = 2
 
 def get_current_state(mat):
 	
